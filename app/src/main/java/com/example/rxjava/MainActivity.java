120dfe6c02ddb1a6d70196cc834c66bb3cb01ac2
Adedoyin <isradeoluwa@gmail.com>
2020-03-31 17:07:52 +0100

    A Game Console Price App that shows the name of the app, price of the game and the date entered into the store.

---
+++ app/src/main/java/com/example/rxjava/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.rxjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.hiText)
+    TextView hiText;
+    @BindView(R.id.myRecyclerView)
+    RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerViewCustomAdapter recyclerViewCustomAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerViewCustomAdapter = new RecyclerViewCustomAdapter();
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(recyclerViewCustomAdapter);
+
+//        Observable.just("Hello, I am dreezy").subscribe(new Consumer<String>() {
+//            @Override
+//            public void accept(String s) throws Exception {
+//                hiText.setText(s);
+//            }
+//        });
+
+        //TextView Text the Lambda Way
+
+        Observable.just("Hey what's up.").subscribe(s -> hiText.setText(s));
+
+//        Observable.just("Android", "Windows", "IOS", "Linux").subscribe(new Consumer<String>() {
+//            @Override
+//            public void accept(String s) throws Exception {
+//                recyclerViewCustomAdapter.addStringToList(s);
+//            }
+//        });
+
+        Entry entry1 = new Entry("PS4", BigDecimal.valueOf(1500), new Date());
+        Entry entry2 = new Entry("Xbox One", BigDecimal.valueOf(2000), new Date());
+        Entry entry3 = new Entry("Xbox One s", BigDecimal.valueOf(2500), new Date());
+        Entry entry4 = new Entry("Xbox One X", BigDecimal.valueOf(3000), new Date());
+
+//        Observable.just(entry1, entry2, entry3, entry4).subscribe(new Consumer<Entry>() {
+//            @Override
+//            public void accept(Entry entry) throws Exception {
+//
+//                recyclerViewCustomAdapter.addEntry(entry);
+//
+//            }
+//        });
+
+        Observable.just(entry1, entry2, entry3, entry4).subscribe(recyclerViewCustomAdapter::addEntry);
+
+    }
+}
---
+++ app/src/main/java/com/example/rxjava/RecyclerViewCustomAdapter.java
@@ -0,0 +1,94 @@
+package com.example.rxjava;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Observable;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecyclerViewCustomAdapter extends RecyclerView.Adapter<RecyclerViewCustomAdapter.MyViewHolder> {
+
+    // An object cannot be created directly from an interface
+    private final List<Entry> entries = new ArrayList<>();
+
+    //Used to inflate the custom row i.e the rv_item.xml filie
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                        // This code helps to inflate our custom row to our recycler view
+        View myView = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_item, parent, false);
+        MyViewHolder myViewHolder = new MyViewHolder(myView);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        //holder.txtName.setText(StringValues.get(position));
+        Entry entry = entries.get(position);
+        holder.setTxtName(entry.getEntryName());
+        holder.setTxtPrice(entry.getEntryPrice());
+        holder.setTxtDate(entry.getEntryDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return entries.size();
+    }
+
+    public void addEntry(Entry entry){
+
+        entries.add(entry);
+        notifyItemInserted(entries.size() - 1);
+
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.txtName)
+        TextView txtName;
+        @BindView(R.id.txtPrice)
+        TextView txtPrice;
+        @BindView(R.id.txtDate)
+        TextView txtDate;
+
+        private final NumberFormat ENTRY_PRICE_FORMAT = new DecimalFormat("$#0.00");
+
+        public void setTxtName(String en) {
+            this.txtName.setText(en);
+        }
+
+        public void setTxtPrice(BigDecimal ep) {
+            this.txtPrice.setText(ENTRY_PRICE_FORMAT.format(ep.doubleValue()));
+        }
+
+        public void setTxtDate(Date ed) {
+            this.txtDate.setText(android.text.format.DateFormat.format("yyyy-MM-dd hh:mm", ed));
+        }
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            //Without this line of code the 'TextView txtName' wont be accessible from here
+            ButterKnife.bind(this, itemView);
+
+        }
+
+    }
+}
